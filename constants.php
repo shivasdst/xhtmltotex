701068a55a72fe4ee75604c81eb0eccd46525c36
prathipoorvi <prathu14@gmail.com>
2018-12-18 10:45:47 +0530

    modified

--- constants.php
+++ constants.php
@@ -1,7 +1,9 @@
 <?php
 
+
 define('ROOT_DIRECTORY', getcwd() . '/');
-define('UNICODE_SRC', '/home/sriranga/Desktop/e-books/srkmys-ebooks/unicode-src/');
+
+define('UNICODE_SRC', '/home/sriranga/Desktop/ebooks/general-ebooks/unicode-src/');
 define('TEXOUT', ROOT_DIRECTORY . 'TeX/');
 define('MAPPING', ROOT_DIRECTORY . 'mapping/');
 
