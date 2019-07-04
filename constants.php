fbf58ea578e019d5848f71e93b102d0949c48326
Bhagyalakshmi <reachrvn@gmail.com>
2019-07-04 18:03:44 +0530

    file modified

--- constants.php
+++ constants.php
@@ -1,13 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-define('ROOT_DIRECTORY', getcwd() . '/');
-define('UNICODE_SRC', '/home/sriranga/Desktop/srkmys-ebooks/unicode-src/');
-=======
-
 define('ROOT_DIRECTORY', '');
-define('UNICODE_SRC', '/home/sriranga/Desktop/ebooks/srkmys-ebooks/unicode-src/');
->>>>>>> a04ca2d0cb80fe34a2f7c563f71c9df857cbe6e7
+define('UNICODE_SRC', '/home/sriranga/Desktop/srkmys-ebooks/unicode-src/');
 define('TEXOUT', ROOT_DIRECTORY . 'TeX/');
 define('MAPPING', ROOT_DIRECTORY . 'mapping/');
 
