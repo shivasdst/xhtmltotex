579f3d18da58744baf91dd0f63917be316c72cf3
Bhagyalakshmi <reachrvn@gmail.com>
2019-07-04 11:12:37 +0530

    File cloned

--- constants.php
+++ constants.php
@@ -1,13 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-
 define('ROOT_DIRECTORY', getcwd() . '/');
 define('UNICODE_SRC', '/home/sriranga/Desktop/srkmys-ebooks/unicode-src/');
-=======
-define('ROOT_DIRECTORY', '');
-define('UNICODE_SRC', '/home/sriranga/Desktop/ebooks/srkmys-ebooks/unicode-src/');
->>>>>>> a83278499dfed2fb00f8c52b7ea3bfad37ad05ae
 define('TEXOUT', ROOT_DIRECTORY . 'TeX/');
 define('MAPPING', ROOT_DIRECTORY . 'mapping/');
 
