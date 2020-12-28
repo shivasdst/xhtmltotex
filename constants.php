7cbfd7172dbb22f4499ea570827ec78b35a70a90
prathipoorvi <prathu14@gmail.com>
2020-12-28 18:03:07 +0530

    modified

--- constants.php
+++ constants.php
@@ -1,8 +1,9 @@
 <?php
 
 define('ROOT_DIRECTORY', '');
-//~ define('UNICODE_SRC', '/home/sriranga/Desktop/ebooks/srkmys-ebooks/unicode-src/');
-define('UNICODE_SRC', '/home/sriranga/Desktop/e-books/general-ebooks/unicode-src/');
+//~ define('UNICODE_SRC', '/home/sriranga/Desktop/ebooks/iasc-eBooks/epub/src/');
+define('UNICODE_SRC', '/home/sriranga/Desktop/ebooks/srkmys-ebooks/unicode-src/');
+//~ define('UNICODE_SRC', '/home/sriranga/Desktop/ebooks/general-ebooks/unicode-src/');
 define('TEXOUT', ROOT_DIRECTORY . 'TeX/');
 define('MAPPING', ROOT_DIRECTORY . 'mapping/');
 
