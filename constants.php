fb6035933a1ca9e01e4c8583f596a89d8860e8ac
prathipoorvi <prathu14@gmail.com>
2021-03-08 16:07:02 +0530

    modified

--- constants.php
+++ constants.php
@@ -2,8 +2,8 @@
 
 define('ROOT_DIRECTORY', '');
 //~ define('UNICODE_SRC', '/home/sriranga/Desktop/ebooks/iasc-eBooks/epub/src/');
-define('UNICODE_SRC', '/home/sriranga/Desktop/ebooks/srkmys-ebooks/unicode-src/');
-//~ define('UNICODE_SRC', '/home/sriranga/Desktop/ebooks/general-ebooks/unicode-src/');
+//~ define('UNICODE_SRC', '/home/sriranga/Desktop/ebooks/srkmys-ebooks/unicode-src/');
+define('UNICODE_SRC', '/home/sriranga/Desktop/ebooks/general-ebooks/unicode-src/');
 define('TEXOUT', ROOT_DIRECTORY . 'TeX/');
 define('MAPPING', ROOT_DIRECTORY . 'mapping/');
 
